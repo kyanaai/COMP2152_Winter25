20da88af04f8c3898bb74129089b2ad8b1860e72
Kiana Nezafat Yazdi <kiananezafat6.kn@gmail.com>
2025-01-19 18:13:16 -0500

    Lab1

---
+++ Labs/Week01/lab1.py
@@ -0,0 +1,23 @@
+#Sample Coding Questions 01 Week 01
+#Kiana Nezafat Yazdi
+#Student ID: 101488042
+
+#Define array
+arr = [1, 4, 7, 9]
+
+#Order of Operations
+a=1
+b=2
+c=3
+d=4
+e = (a - ((b ** c) // d) + (a % c))
+print(e)
+
+#formatting
+temperature=32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+#Common Functions
+userAge = int(input("Please enter your age: "))
+userAge += 22
+print("Now showing the shop items filtered by age:", userAge)
